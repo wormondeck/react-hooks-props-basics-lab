e9c4fbaf2f637d226c57b7da76dd914fb004283f
Ronney Arias <ronneyarias@gmail.com>
2024-02-02 14:34:00 -0600

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
 function App() {
+  const githubLink = "https://github.com/liza";
+  const linkedinLink = "https://www.linkedin.com/in/liza/";
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
